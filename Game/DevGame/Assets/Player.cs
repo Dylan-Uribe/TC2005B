7b1c9c3a652a48baec2091164a3c402a7c0325ba
dylan <a2022112008@estudiantes.upsa.edu.bo>
2025-08-18 12:50:57 -0600

    add walk movement to apple player

--- Game/DevGame/Assets/Player.cs
+++ Game/DevGame/Assets/Player.cs
@@ -1,3 +1,4 @@
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 //El nombre de la clase debe ser el mismo del de Unity
@@ -5,13 +6,17 @@ public class Player : MonoBehaviour
 {
     //Inicializar variables
     Animator animator;
-
+    Rigidbody2D body;
+    float mov;
+    bool seeRight = true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //(conectar todas las propiedades del animator)
         animator = GetComponent<Animator>();
+        body = GetComponent<Rigidbody2D>();
+        //Busca en tu inspector un componente RidigBody
     }
 
     // Update is called once per frame
@@ -22,5 +27,27 @@ public class Player : MonoBehaviour
         {
             animator.Play("PlayerPunch");
         }
+
+        mov = Input.GetAxis("Horizontal");
+        animator.SetFloat("MOV", Mathf.Abs(mov));
+        body.linearVelocity = new Vector2(mov*5,body.linearVelocity.y);
+
+        if (seeRight && mov < 0) 
+        {
+            Flip();
+        }
+
+        if (!seeRight && mov > 0) 
+        {
+            Flip();
+        }
+    }
+
+    void Flip() 
+    {
+        seeRight = !seeRight;
+        Vector3 scale = transform.localScale; //valores del transform de x,y,z
+        scale.x *= -1;
+        transform.localScale = scale;
     }
 }
