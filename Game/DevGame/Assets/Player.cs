86d435543bb221fa273dba03eb1aa24561750f0a
dylan <a2022112008@estudiantes.upsa.edu.bo>
2025-08-19 13:15:36 -0600

    add jump animation

--- Game/DevGame/Assets/Player.cs
+++ Game/DevGame/Assets/Player.cs
@@ -1,5 +1,6 @@
 using UnityEditor.Tilemaps;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 //El nombre de la clase debe ser el mismo del de Unity
 public class Player : MonoBehaviour
@@ -9,27 +10,42 @@ public class Player : MonoBehaviour
     Rigidbody2D body;
     float mov;
     bool seeRight = true;
+    public bool jump = true;
+    public Transform feet;
+    float fuerzaSalto = 50f;
+    public LayerMask whatIsGround;
+    float feetSize = 0.2f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //(conectar todas las propiedades del animator)
         animator = GetComponent<Animator>();
-        body = GetComponent<Rigidbody2D>();
         //Busca en tu inspector un componente RidigBody
+        body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     //Ejecutado cada cuadro por segundo (60)
     void Update()
     {
+        //Unity evalua si 2 objetos están colisionando.
+        jump = !Physics2D.OverlapCircle(feet.position, whatIsGround);
+
         if (Input.GetKey(KeyCode.K)) 
         {
             animator.Play("PlayerPunch");
         }
 
+        if (Input.GetKey(KeyCode.UpArrow) && !jump) 
+        {
+            body.AddForce(new Vector2(0f, fuerzaSalto));         
+        }
+
         mov = Input.GetAxis("Horizontal");
         animator.SetFloat("MOV", Mathf.Abs(mov));
+        animator.SetBool("JUMP", jump);
+        animator.SetFloat("ALTURA", body.linearVelocity.y);
         body.linearVelocity = new Vector2(mov*5,body.linearVelocity.y);
 
         if (seeRight && mov < 0) 
